bbf09a95fab199ea68f95eb958fb7e933fc67fca
Shreya Yadav <yadavshreya141@gmail.com>
2024-01-20 15:52:51 +0530

    message

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ menuItems.forEach((item, index) => {
 
     //change texts of currentProduct
     currentProductTitle.textContent = choosenProduct.title;
-    currentProductPrice.textContent = `Rs. + ${choosenProduct.price}`;
+    currentProductPrice.textContent =` Rs.  ${choosenProduct.price}`;
     currentProductImg.src = choosenProduct.colors[0].img;
 
     //assing new colors
